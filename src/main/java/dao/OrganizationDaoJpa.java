e8b62c3ca7fc15b1c835cfe16e16d6943160732b
kaden89 <denisufaltc@gmail.com>
2017-01-13 18:01:40 +0500

    Organization DAO fix

--- src/main/java/dao/OrganizationDaoJpa.java
+++ src/main/java/dao/OrganizationDaoJpa.java
@@ -11,44 +11,6 @@ import java.util.List;
  * Created by dkarachurin on 13.01.2017.
  */
 @Stateless
-public class OrganizationDaoJpa implements OrganizationDAO {
-    @PersistenceContext(unitName="EcmPersistence")
-    private EntityManager entityManager;
+public class OrganizationDaoJpa extends GenericDaoJpa<Organization> {
 
-    public OrganizationDaoJpa() {
-    }
-
-    public EntityManager getEntityManager() {
-        return entityManager;
-    }
-
-    public void setEntityManager(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
-    @Override
-    public List<Organization> findAll() {
-        return entityManager.createQuery("SELECT o FROM Organization o").getResultList();
-    }
-
-    @Override
-    public Organization find(int id) {
-        return entityManager.find(Organization.class, id);
-    }
-
-    @Override
-    public void addOrganization(Organization organization) {
-        entityManager.persist(organization);
-    }
-
-    @Override
-    public void updateOrganization(Organization organization) {
-        entityManager.merge(organization);
-    }
-
-    @Override
-    public void deleteOrganization(int id) {
-        Organization organization = find(id);
-        entityManager.remove(organization);
-    }
 }
